db75a4f46b1395887832ddfa5dcd80808ce8d89a
wanlerong <1134654196@qq.com>
2022-09-02 08:57:19 +0800

    feat: splitRange

--- src/editor.ts
+++ src/editor.ts
@@ -15,7 +15,35 @@ class Editor {
 
   bold() {
     let range: Range = this.getSelectionRange()
+    this.splitRange(range)
+    console.log(range)
+  }
 
+  splitRange(range: Range) {
+    // 假设选中的是 text 节点，parentElement is span
+    let start: HTMLElement = range.startContainer.parentElement
+    let newNode = document.createElement(start.nodeName)
+    let beforeText = start.innerText.substring(0, range.startOffset);
+    let afterText = start.innerText.substring(range.startOffset);
+    if (beforeText !== '' && afterText !== '') {
+      newNode.innerText = beforeText;
+      start.innerText = afterText;
+      start.parentNode.insertBefore(newNode, start)
+    }
+
+    let end: HTMLElement = range.endContainer.parentElement
+    newNode = document.createElement(end.nodeName)
+    let endOffset = range.endOffset
+    beforeText = end.innerText.substring(0, range.endOffset);
+    afterText = end.innerText.substring(range.endOffset);
+    if (beforeText !== '' && afterText !== '') {
+      newNode.innerText = afterText;
+      end.innerText = beforeText;
+      end.parentNode.insertBefore(newNode, end.nextSibling)
+    }
+
+    range.setStart(start.firstChild, 0)
+    range.setEnd(end.firstChild, endOffset)
   }
 
   hi(): void {
